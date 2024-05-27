e645d5299cdeccba311c1ea243f35ecbd0d38445
Ganpati kumar <151036000+Ganpati-kumar@users.noreply.github.com>
2024-05-27 18:26:13 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+// alert (3 + 3) // we are using nodejs, not browser
+
+// MDN or ECMASCRIPT TO KNOW ABOUT ANY DOCUMENTATION
+
+
+let name = " ganpati"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// datatypes are
+
+//number => 2 to power 53
+// bigint
+//string => ""
+// boolean => true/false
+//null => standalone vaiue
+//undefined =>  
+//symbol => unique
+
+//object
+
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);     //object
