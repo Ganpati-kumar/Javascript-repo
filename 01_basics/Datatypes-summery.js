089074a82a3b51b45034870f292c548cba0d02ce
Ganpati kumar <151036000+Ganpati-kumar@users.noreply.github.com>
2024-05-28 10:07:46 +0000

    change in location of file

---
+++ 01_basics/Datatypes-summery.js
@@ -0,0 +1,27 @@
+// Primitive 
+ // types : String, Number, Boolean,null,undefined, Symbol , BigInt 
+
+const bigNumber = 3142532625n // n use for big int 
+
+
+ // Reference (Non primitive)
+   types: Array, Object , Function
+
+   const hero = [" ganpati","hjdj" , "sdfss"] ; // arrays
+
+   let myObj = {                  // objects
+        name: "ganpati",   
+        age:23
+   }
+
+   const myFunction = function(){              // function
+    console.log("Hello Duggu");
+   }
+   
+
+
+   //to know datatype - use typeof function
+
+   console.log(typeof bigNumber);// bigint
+   // undefined -  undefined
+   // Null -        object
