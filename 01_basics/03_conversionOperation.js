5d4028bc5afd6c99a0d0ef8b8238037e2d542000
Ganpati kumar <151036000+Ganpati-kumar@users.noreply.github.com>
2024-05-28 08:59:45 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,5 +24,41 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log( stringNumber)
-console.log( typeof stringNumber)
+// console.log( stringNumber)
+// console.log( typeof stringNumber)
+
+// ***************** Operation ***********//
+
+let value = 3
+let negValue = - value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " ganpati"
+
+let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log("1" + 2);  //12
+
+// console.log(1+ "2");   // 12
+
+// console.log("1" + 2 + 2);  //122
+
+// console.log(1+ 2 +"2");  //32
+
+
+// console.log(5 == false) //false
+// console.log(true);   // true
+// console.log(+true)   //1
+
+// console.log(+"");  // 0
+
+
+let gameCounter = 100
+++gameCounter;
+gameCounter++;
+console.log(gameCounter); //102
+gameCounter++;
+console.log(gameCounter); //103
