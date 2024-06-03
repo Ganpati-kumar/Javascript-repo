c2c1a7a4c8a1de2a5e66d21d6437c965e3d6e0f1
Ganpati kumar <151036000+Ganpati-kumar@users.noreply.github.com>
2024-06-03 18:28:00 +0000

    if else

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -5,44 +5,44 @@
 // }
 
 if ( 2 == " 2") {
-   // console.log("executed");    // run 
+    console.log("executed");    // run 
 }
 // === checked datatype also 
 
 if ( 2 === "2") {
-   // console.log("executed");    // not equal
+    console.log("executed");    // not equal
 }
-//console.log("not equal");
+console.log("not equal");
 
 //+=========================== if else=======================//
-//  const temperature = 42
+ const temperature = 42
  
-//  if (temperature === 41){
-//     console.log("less than current temperature");
-//  } else {
-//     console.log(" greater");
-//  }
+ if (temperature === 41){
+    console.log("less than current temperature");
+ } else {
+    console.log(" greater");
+ }
 
-// const score = 200
-// if ( score > 100){
-//     const power = " fly"   // if we use var then their scope is global .,,this is not good
-//     console.log(`user power ${power}`);  // user power  fly
-// }
-//console.log(`user power ${power}`); //  power is not defined // outside the scope
+const score = 200
+if ( score > 100){
+    const power = " fly"   // if we use var then their scope is global .,,this is not good
+    console.log(`user power ${power}`);  // user power  fly
+}
+console.log(`user power ${power}`); //  power is not defined // outside the scope
 
 // implicit scope ++++++++++++++++++++++++
-// const balance = 1000
-// if (balance > 500) console.log("test"); // test
+const balance = 1000
+if (balance > 500) console.log("test"); // test
  
 //++++++++++++++++++++ if else and else if+++++++++
 
-// if ( balance < 500) {
-//     console.log("less");
-// } else if ( balance < 750) {
-//     console.log("less than 750");
-// }  else if ( balance < 950) {
-//     console.log("less than 950");
-// } else {
-//     console.log("less than 1000"); // less than 1000
-// }
+if ( balance < 500) {
+    console.log("less");
+} else if ( balance < 750) {
+    console.log("less than 750");
+}  else if ( balance < 950) {
+    console.log("less than 950");
+} else {
+    console.log("less than 1000"); // less than 1000
+}
 
