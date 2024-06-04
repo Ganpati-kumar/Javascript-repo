fbfd389381640798d7134c913e672e7caf270a8d
Ganpati kumar <151036000+Ganpati-kumar@users.noreply.github.com>
2024-06-04 16:52:12 +0000

    while and do while loop

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -1,18 +1,18 @@
 //+++++++++++ while loop ++++++++++++++++++++++++//
 
 
-// let index = 0 
-// while (index <= 10) {
-//     console.log(`Value of index is ${index}`);
-//     index = index + 2
-// }
+let index = 0 
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
 
-//   let myArray = [ " sfg","sdfdv","fsvsfsv"]
-//   let arr = 0
-//   while (arr < myArray.length) {
-//     console.log(`value of ${myArray[arr]}`)
-//         arr = arr + 1
-//   }
+  let myArray = [ " sfg","sdfdv","fsvsfsv"]
+  let arr = 0
+  while (arr < myArray.length) {
+    console.log(`value of ${myArray[arr]}`)
+        arr = arr + 1
+  }
 
 // ++++++++++++++++++++  do while loop ++++++++++++++++++++++++++
  let score = 1
